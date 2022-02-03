53def9d06c3a677d93c39d7e9409b50a7609e485
macbookair <dailydevo9@gmail.com>
2022-02-03 02:04:30 +0100

    fffd

--- routes/vendors.js
+++ routes/vendors.js
@@ -42,10 +42,29 @@ router.get("/vendor/clear", function (req, res, next) {
     .catch(next);
 });
 
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
 //api to get all vendors
 router.get("/vendor", function (req, res, next) {
   Vendor.find({})
     .then(function (vendor) {
+      shuffle(vendor);
       res.send(vendor);
     })
     .catch(next);
